0e56e26d9076df8a975025876f70c655e11896f7
usamec <usama@ksp.sk>
2012-12-11 17:51:13 +0100

    Pridany ukazkovy kod, co to zvlada.

---
+++ example.c
@@ -0,0 +1,17 @@
+int printf(char *x, ...);
+
+int add(int a, int b) {
+  return a + b + 47;
+}
+
+int main() {
+  int b[2][2];
+  b[0][0] = 1;
+  b[1][0] = 42;
+  int i;
+  i = 0;
+  while (i < 7) {
+    printf("%d\n", add(b[0][0], b[1][0]));
+    i = i + 1;
+  }
+}
