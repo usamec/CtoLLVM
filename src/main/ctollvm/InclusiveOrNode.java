8f64548b94d8d4a40de07f51e09498f8cc3cf200
evaku <ekubascikova@gmail.com>
2012-12-14 03:59:37 -0800

    pridany ^ |

---
+++ src/main/ctollvm/InclusiveOrNode.java
@@ -0,0 +1,26 @@
+package ctollvm;
+import java.io.PrintStream;
+import java.lang.Exception;
+
+public class InclusiveOrNode implements PNode {
+  private PNode lhs;
+  private PNode rhs;
+
+  public InclusiveOrNode(PNode lhs, PNode rhs) {
+    this.lhs = lhs;
+    this.rhs = rhs;
+  }
+
+  @Override
+  public EvalResult produceOutput(PrintStream out) throws Exception { 
+    EvalResult l = lhs.produceOutput(out);
+	  EvalResult r = rhs.produceOutput(out);
+	  
+	  EvalResult res = new EvalResult(l.type);
+	  
+	  out.println(String.format("%s = or %s %s, %s", res.getRepresentation(), l.type.getRepresentation(),l.getRepresentation(), r.getRepresentation()));
+	  
+    
+    return res;
+  }
+}
