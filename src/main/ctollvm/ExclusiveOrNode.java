63ee4748d877de1cf774bf4bd1f4052aa908142b
evaku <ekubascikova@gmail.com>
2012-12-14 04:00:08 -0800

    pridany ^ |

---
+++ src/main/ctollvm/ExclusiveOrNode.java
@@ -0,0 +1,26 @@
+package ctollvm;
+import java.io.PrintStream;
+import java.lang.Exception;
+
+public class ExclusiveOrNode implements PNode {
+  private PNode lhs;
+  private PNode rhs;
+
+  public ExclusiveOrNode(PNode lhs, PNode rhs) {
+    this.lhs = lhs;
+    this.rhs = rhs;
+  }
+
+  @Override
+  public EvalResult produceOutput(PrintStream out) throws Exception { 
+    EvalResult l = lhs.produceOutput(out);
+	  EvalResult r = rhs.produceOutput(out);
+	  
+	  EvalResult res = new EvalResult(l.type);
+	  
+	  out.println(String.format("%s = xor %s %s, %s", res.getRepresentation(), l.type.getRepresentation(),l.getRepresentation(), r.getRepresentation()));
+	  
+    
+    return res;
+  }
+}
